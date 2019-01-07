4bc2ca7d81ec44fe74540d9b8ffc41592474448f
pk100101 <42978490+pk100101@users.noreply.github.com>
2019-01-06 22:11:43 -0500

    Add files via upload

--- src/BankAccountMain.java
+++ src/BankAccountMain.java
@@ -22,28 +22,66 @@ public class BankAccountMain
 			String response = s.nextLine();
 			if ((response.toLowerCase()).equals("add an account"))
 			{
-				response1=false;
-				System.out.print("What kind of account would you like to add?"
+				System.out.println("What kind of account would you like to add?"
 						+ "\nYou can create a checking or a savings account");
-				s.nextLine();
 				response = s.nextLine();	
-				if (response.equals("checking"))
+				if ((response.toLowerCase()).equals("checking"))
 					{
 						System.out.println("Please provide a name");
 						String name = s.nextLine();
-						s.nextLine();
+						System.out.println("Name: " + name);
 						System.out.println("Would you like to provide an initial balance?"
 								+ "\nYes or No");
+						response = s.nextLine();
+						if ((response.toLowerCase()).equals("yes"))
+						{
+							System.out.println("You have chosen to provide an initial balance"
+									+ "\nPlease enter your initial balance");
+							double balance = s.nextDouble();
+							s.nextLine();
+							System.out.println("Balance: " + balance);
+							accounts.add(new CheckingAccount (name, balance, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+							accounts.get(0).toString();
+						}
+						else if ((response.toLowerCase()).equals("no"))
+						{
+							accounts.add(new CheckingAccount (name, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+							accounts.get(0).toString();
+						}
 					}
-					else if (response.equals("savings"))
+					else if ((response.toLowerCase()).equals("savings"))
 					{
-						
+						System.out.println("Please provide a name");
+						String name = s.nextLine();
+						System.out.println("Name: " + name);
+						System.out.println("Would you like to provide an initial balance?"
+								+ "\nYes or No");
+						s.nextLine();
+						if ((response.toLowerCase()).equals("yes"))
+						{
+							System.out.println("You have chosen to provide an initial balance"
+									+ "\nPlease enter your initial balance");
+							s.nextLine();
+							double balance = s.nextDouble();
+							accounts.add(new SavingsAccount (name, balance, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+						}
+						else if ((response.toLowerCase()).equals("no"))
+						{
+							accounts.add(new SavingsAccount (name, OVER_DRAFT_FEE, TRANSACTION_FEE, FREE_TRANSACTIONS));
+							accounts.toString();
+						}
+					}
+					else
+					{	
+						System.out.println("Invalid response, try again");
+						s.nextLine();
 					}
 			}
 			else if (response.equals("make a transaction"))
 			{
 				System.out.println("What kind of transaction would you like to make"
 						+ "\nYou can withdraw, deposit, transfer, or get account numbers");
+				s.nextLine();
 				if ((response.toLowerCase()).equals("withdraw"))
 				{
 					System.out.println("Please enter your account number");
@@ -54,6 +92,7 @@ public class BankAccountMain
 				{
 					System.out.println("Please enter your account number");
 					//finish up!
+					s.nextInt();
 				}
 				else if ((response.toLowerCase()).equals("transfer"))
 				{
@@ -62,7 +101,10 @@ public class BankAccountMain
 				}
 			}
 			else if ((response.toLowerCase()).equals("terminate the program"))
-				response1 = false;
+				{
+					System.out.println("Thank you for banking with us, we hope to see you soon");
+					response1 = false;
+				}
 			else
 				System.out.println("Invalid response, please try again");
 		}
